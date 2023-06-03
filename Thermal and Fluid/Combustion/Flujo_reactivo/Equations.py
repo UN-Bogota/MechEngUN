9c1b458f248f218c47bd01f91d3586c53be6a4e9
Julián Esteban Olejua Pinto <jolejua@unal.edu.co>
2023-06-03 16:54:41 -0500

    Ensamble de sistema de ecuaciones

--- Thermal and Fluid/Combustion/Flujo_reactivo/Equations.py
+++ Thermal and Fluid/Combustion/Flujo_reactivo/Equations.py
@@ -6,15 +6,40 @@ Created on Tue May 30 18:15:21 2023
 @author: julian
 """
 import numpy as np
+L=1
 n=10
 dx=L/(n-1)
 especies=['','','','','','','']
-cells=n*(3*len(especies)+1)
-A=np.zeros([cells])
-C=np.zeros([cells,1])
-Av=np.zeros([len(especies)])
-for i in range(especies):
-    for j in range(especies):
-            Av[i,i]+=-X[i]*X[j]/D[i,j]
-            Av[i,j]+=X[i]*X[j]/D[i,j]
+
+def create_A(Y):
+    l=len(especies)
+    cells=n*(l+1)
+    A=np.zeros([cells])
+    C=np.zeros([cells,1])
+    #Av=np.zeros([len(especies),len(especies)])
+    for m in range(1,n):
+        MWm=1/(np.dot(Y, 1/MW))
+        for i in range(l):
+            #Término convectivo
+            A[n*i+m,n*i+m]=rho*v/dx
+            A[n*i+m,n*i+m-1]=-rho*v/dx
+            C[n*i+m]=w(Y,T)
+            for j in range(l):
+                #Término difusivo
+                if not m==(n-1):
+                    #No hay difusión en la salida
+                    A[n*i+m,j*n+m]+=2*rho*MW[i]/(MWm*dx**2)*Dm[i,j]
+                    A[n*i+m,j*n+m+1]+=-rho*MW[i]/(MWm*dx**2)*Dm[i,j]
+                    A[n*i+m,j*n+m-1]+=-rho*MW[i]/(MWm*dx**2)*Dm[i,j]
+        A[l*n+m,m]=1
+        C[l*n+m]=P/(rho*R)*MWm
+    #BC
+    for i in range(l):
+        A[n*i,n*i]=1
+        C[n*i]=x0[i]
+#Arreglar Y para que sea el valor en la celda
+#Añadir el código de solución
+
+        
+
     

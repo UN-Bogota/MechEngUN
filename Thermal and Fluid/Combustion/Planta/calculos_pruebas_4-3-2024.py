5bddd448ab162e03d4f6ceff6040b944f037e600
Sebastian Forero <seforeros@gmail.com>
2024-03-13 00:15:15 +0000

    calculos potencia_elec, eficiencia_gen, potencia_freno, bmep

--- Thermal and Fluid/Combustion/Planta/calculos_pruebas_4-3-2024.py
+++ Thermal and Fluid/Combustion/Planta/calculos_pruebas_4-3-2024.py
@@ -20,7 +20,54 @@ class ExcelReader:
         except Exception as e:
             print(f"Error: {e}")
             return None
+        
+# Create a class to represent the DataFrame as an object
+class df_to_object:
+    def __init__(self, df):
+        self.rpm = df['rpm'] #rpm
+        self.Vi = df['Vi'] #V
+        self.Vc = df['Vc'] #V
+        self.Vd = df['Vd'] #V
+        self.Ii = df['Ii'] #A
+        self.Ic = df['Ic'] #A
+        self.Id = df['Id'] #A
+        self.T_ingreso = df['T_ingreso'] #°C
+        self.P_ingreso = df['P_ingreso'] #psi
+        self.flujo_vol_1 = df['flujo_vol_1'] #lpm
+        self.flujo_vol_2 = df['flujo_vol_2'] #lpm
+        self.t_inyeccion = df['t_inyeccion'] #ms
+        self.flujo_masico = df['flujo_masico'] #g/s
 
-diesel = ExcelReader('pruebas_lab.xlsx', 'CH4_1', 42, 49, 'C:O').read_data()
+def perform_calc(df, boolean):
 
-print(diesel)
+  nc = 2.0 # Number of revolutions per work cycle
+  Vd = 406e-6 # Displaced Volume m3
+  rho_B10 = 838.00 # kg/m3
+  LHV_B10 = 46079.97 # kJ/kg
+  LHV_GNV = 42188.30 # kJ/m3
+
+  # Exponential association for the generator y = a(1-exp(-bx))
+  a, b = 0.7560299, 0.005690096
+
+  df.potencia_elec = (df.Vi*df.Ii + df.Vc*df.Ic + df.Vd*df.Id)/3/1000 # kW
+  df.eficiencia_gen = a*(1-np.exp(-b*1000*df.potencia_elec)) # -
+  df.potencia_freno = df.potencia_elec/df.eficiencia_gen # kW
+  df.bmep = (df.potencia_freno*nc)/(Vd*df.rpm/60) # kPa
+  
+  
+  #df.energia_calorifica1 = energia_calor_Diesel(LHV_B10, df.rpm, df.flujo_masico)
+
+  #df.eff_termica1 =
+
+  return df
+
+
+filename = '/workspaces/MechEngUN/Thermal and Fluid/Combustion/Planta/pruebas_lab.xlsx'
+
+diesel = df_to_object(ExcelReader(filename, 'CH4_1', 43, 48, 'C:O').read_data())
+dual1 = df_to_object(ExcelReader(filename, 'CH4_1', 50, 55, 'C:O').read_data())
+dual2 = df_to_object(ExcelReader(filename, 'CH4_1', 57, 62, 'C:O').read_data())
+
+diesel = perform_calc(diesel, True)
+
+print(diesel.eficiencia_gen)

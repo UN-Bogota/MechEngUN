4fb8f622e739d3838b99410ca3125f265f64b0be
jolejua <jolejua@unal.edu.co>
2024-03-15 21:24:46 -0500

    Modificación flujos de metano

--- Thermal and Fluid/Combustion/Planta/calculos_pruebas_4-3-2024.py
+++ Thermal and Fluid/Combustion/Planta/calculos_pruebas_4-3-2024.py
@@ -7,6 +7,8 @@ Created on Tue Mar 12 21:40:43 2024
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+from matplotlib.ticker import MultipleLocator
 
 class ExcelReader:
     def __init__(self, filename, sheet_name, start_row, end_row, columns_to_read):
@@ -56,29 +58,37 @@ def perform_calc(df, dual):
     rho_B10 = 838.00 # kg/m3
     LHV_B10 = 46079.97 # kJ/kg
     LHV_GNV = 42188.30 # kJ/m3
+    LHV_GNVm = 42619 #kJ/kg
 
     # Exponential association for the generator y = a(1-exp(-bx))
     a, b = 0.7560299, 0.005690096
 
-    df.potencia_elec = (df.Vi*df.Ii + df.Vc*df.Ic + df.Vd*df.Id)/1000 # kW
-    df.eficiencia_gen = a*(1-np.exp(-b*1000*df.potencia_elec)) # -
-    df.potencia_freno = df.potencia_elec/df.eficiencia_gen # kW
-    df.bmep = (df.potencia_freno*nc)/(Vd*df.rpm/60) # kPa
+    df['potencia_elec'] = (df.Vi*df.Ii + df.Vc*df.Ic + df.Vd*df.Id)/1000 # kW
+    df['eficiencia_gen'] = a*(1-np.exp(-b*1000*df.potencia_elec)) # -
+    df['potencia_freno'] = df.potencia_elec/df.eficiencia_gen # kW
+    df['bmep'] = (df.potencia_freno*nc)/(Vd*df.rpm/60) # kPa
+    df['flujo_vol']=df[['flujo_vol_1', 'flujo_vol_2']].mean(axis=1)#L/min
+    
+    
+    #if dual:
+    #df['energía_GNV']=(df.flujo_vol/1000/60*df.t_inyeccion/1000*LHV_GNV)
+    #df['energia_calorifica'] = (df.flujo_vol/1000/60*df.t_inyeccion/1000*LHV_GNV) + (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
+    df['energía_Diesel']=(df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
+    df['energía_GNV']=df.t_inyeccion*2/1000/1000*LHV_GNVm*0.4
+    df['energia_calorifica'] = df['energía_Diesel']+df['energía_GNV']
+        #df['energia_calorifica2'] = (df.flujo_vol_2/1000/60*df.t_inyeccion/1000*LHV_GNV) + (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
 
-    if dual:
-        df.energia_calorifica1 = (df.flujo_vol_1/1000/60*df.t_inyeccion/1000*LHV_GNV) + (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
+    df['sustitucion'] =  df['energía_GNV']/df['energia_calorifica']#(df.flujo_vol/1000/60*df.t_inyeccion/1000*LHV_GNV)/df.energia_calorifica
+        #df['sustitucion2'] = (df.flujo_vol_2/1000/60*df.t_inyeccion/1000*LHV_GNV)/df.energia_calorifica2
+    # else: 
+    #     df['energia_calorifica1'] = (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
 
-        df.energia_calorifica2 = (df.flujo_vol_2/1000/60*df.t_inyeccion/1000*LHV_GNV) + (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
-
-        df.sustitucion1 = (df.flujo_vol_1/1000/60*df.t_inyeccion/1000*LHV_GNV)/df.energia_calorifica1
-        df.sustitucion2 = (df.flujo_vol_2/1000/60*df.t_inyeccion/1000*LHV_GNV)/df.energia_calorifica2
-    else: 
-        df.energia_calorifica1 = (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
-
-        df.energia_calorifica2 = (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
-
-    df.eff_termica1 = df.bmep*Vd/df.energia_calorifica1
-    df.eff_termica2 = df.bmep*Vd/df.energia_calorifica2
+    #     df['energia_calorifica2'] = (df.flujo_masico/1000)*2*LHV_B10/(df.rpm/60)
+    
+    df['eff_termica'] = df.bmep*Vd/df.energia_calorifica
+    df['flujo_energetico']=df.energia_calorifica/2*df.rpm #kJ/min
+    df['consumo_especifico']=(df['energía_GNV']/LHV_GNVm*1000+df['energía_Diesel']/LHV_B10*1000)/(df.bmep*Vd)*3600#g/kWh
+    #df['eff_termica2'] = df.bmep*Vd/df.energia_calorifica2
 
     return df
 
@@ -87,9 +97,13 @@ def perform_calc(df, dual):
 filename = 'pruebas_lab.xlsx'
 #filename = '/workspaces/MechEngUN/Thermal and Fluid/Combustion/Planta/pruebas_lab.xlsx'
 
-diesel = df_to_object(ExcelReader(filename, 'CH4_1', 43, 48, 'C:O').read_data())
-dual1 = df_to_object(ExcelReader(filename, 'CH4_1', 50, 55, 'C:O').read_data())
-dual2 = df_to_object(ExcelReader(filename, 'CH4_1', 57, 62, 'C:O').read_data())
+diesel = ExcelReader(filename, 'CH4_1',43, 48, 'C:O').read_data()
+dual1 = ExcelReader(filename, 'CH4_1', 50, 55, 'C:O').read_data()
+dual2 = ExcelReader(filename, 'CH4_1', 57, 62, 'C:O').read_data()
+
+prueba2=pd.read_excel(filename, 'CH4_1', skiprows= 4, nrows= (41-6+1), usecols='A:R')
+prueba2['flujo_masico']=(prueba2.m_0-prueba2.m_60)/60
+prueba2 = perform_calc(prueba2, True)
 
 diesel = perform_calc(diesel, False)
 dual1 = perform_calc(dual1, True)
@@ -98,7 +112,7 @@ dual2 = perform_calc(dual2, True)
 # Graficas
 
     # Eficiencia termica ------------------------------------------------------
-
+"""
 plt.figure(figsize=(8*1.5, 6*1.5))
 
 plt.plot(diesel.bmep, 100*diesel.eff_termica1, color = 'black', label='Diesel')
@@ -107,7 +121,7 @@ dual1_eff_avg = 100*(dual1.eff_termica1 + dual1.eff_termica2)/2
 dual2_eff_avg = 100*(dual2.eff_termica1 + dual2.eff_termica2)/2
 
 plt.plot(dual1.bmep, dual1_eff_avg, color = 'blue', label='Dual1')
-plt.fill_between(dual1.bmep, 100*dual1.eff_termica1, 100*dual1.eff_termica2,
+#plt.fill_between(dual1.bmep, 100*dual1.eff_termica1, 100*dual1.eff_termica2,
                  color='blue', alpha=0.2, label=None)
 
 plt.plot(dual2.bmep, dual2_eff_avg, color = 'red', label='Dual2')
@@ -150,3 +164,44 @@ plt.grid(True)  # Add grid lines
 
 
 plt.show()
+"""
+fig, ax1 = plt.subplots()
+sns.lineplot(data=prueba2,x='carga',y=100*prueba2['eff_termica'],hue='mapa')
+ax1.set(ylabel='%$\eta_b$')
+ax2 = ax1.twiny()
+sns.lineplot(data=prueba2,x='bmep', y='eff_termica', visible=False)
+
+fig, ax1 = plt.subplots()
+#prueba2['Sustitución']= prueba2[['sustitucion1', 'sustitucion2']].mean(axis=1)
+sns.lineplot(data=prueba2,x='carga',y=100*prueba2['sustitucion'],hue='mapa')
+ax1.set(ylabel='% Sustitución $CH_4$')
+ax2 = ax1.twiny()
+sns.lineplot(data=prueba2,x='bmep', y='sustitucion', visible=False)
+
+fig, ax1 = plt.subplots()
+sns.lineplot(data=prueba2,x='carga',y='flujo_energetico',hue='mapa')
+ax1.set(ylabel='Flujo energético [kJ/min]')
+ax2 = ax1.twiny()
+sns.lineplot(data=prueba2,x='bmep', y='flujo_energetico', visible=False)
+
+fig, ax1 = plt.subplots()
+sns.lineplot(data=prueba2,x='carga',y='consumo_especifico',hue='mapa')
+ax1.set(ylabel='Consumo específico [g/kW-h]')
+ax2 = ax1.twiny()
+sns.lineplot(data=prueba2,x='bmep', y='consumo_especifico', visible=False)
+
+#ax2.spines['top'].set_position(('axes', -0.15))
+#ax2.spines['top'].set_visible(False)
+plt.tick_params(which='both', top=False)
+
+fig, ax1 = plt.subplots()
+sns.lineplot(data=prueba2,x='carga',y='potencia_freno',hue='mapa')
+ax1.set(ylabel='Potencia al freno [kW]')
+ax2 = ax1.twiny()
+sns.lineplot(data=prueba2,x='bmep', y='potencia_freno', visible=False)
+
+fig, ax1 = plt.subplots()
+sns.lineplot(data=prueba2,x='carga',y='bmep',hue='mapa')
+ax1.set(ylabel='bmep [kPa]')
+
+
